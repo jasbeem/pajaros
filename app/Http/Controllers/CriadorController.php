a6b13e5a8b0f29fe5113923b185c492efa3d15a4
Jose Antono Sánchez <jasbeem@gmail.com>
2022-05-26 13:29:13 +0200

    Creacion de los modelos y los controladores

---
+++ app/Http/Controllers/CriadorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Criador;
+use Illuminate\Http\Request;
+
+class CriadorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Criador  $criador
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Criador $criador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Criador  $criador
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Criador $criador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Criador  $criador
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Criador $criador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Criador  $criador
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Criador $criador)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PuestaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Puesta;
+use Illuminate\Http\Request;
+
+class PuestaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Puesta  $puesta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Puesta $puesta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Puesta  $puesta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Puesta $puesta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Puesta  $puesta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Puesta $puesta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Puesta  $puesta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Puesta $puesta)
+    {
+        //
+    }
+}
